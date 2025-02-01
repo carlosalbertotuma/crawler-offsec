19a265117e96815616d1a6e5b650b8fdaf86a5c5
Bl4dSc4n <carlosalbertotuma@yahoo.com.br>
2025-02-01 18:04:10 -0300

    Add files via upload

---
+++ crawler-offsec.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+banner()
+{
+ echo '+-+-+-+-+-+-+-+ +-+-+-+-+-+-+'
+ echo '|C|r|a|w|l|e|r| |O|f|f|S|e|c|'
+ echo '+-+-+-+-+-+-+-+ +-+-+-+-+-+-+'
+ echo 'Desenvolvido por bl4dsc4n'
+}
+banner
+wget2 --spider -r ${1} |  grep "Adding URL:" | cut -d " " -f 3 | tee ${1}.txt 
+echo "arquivos interessantes"
+URLS=$(cat ${1}.txt | grep -oP "\b([a-zA-Z0-9-]+\.)+${1}\b" | sort -u)
+echo $URLS
+for i in $URLS; do wget2 --tries=1 --connect-timeout=10 --read-timeout=10 --timeout=20 --spider -r ${i} |  grep "Adding URL:" | cut -d " " -f 3 | tee ${1}_full.txt; done
